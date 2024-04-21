b8afddd53038c738683129f878d6cf0a51612562
MohsinShah12345 <mohsinking381@gmail.com>
2024-04-21 01:17:48 -0700

    Shub Lab second  commit

--- src/modules/counter/actions.js
+++ src/modules/counter/actions.js
@@ -2,7 +2,7 @@ export const INCREMENT = 'INCREMENT';
 export const DECREMENT = 'DECREMENT';
 
 
-export const increment = (payload) => ({
+export const increment = () => ({
     type: INCREMENT
 });
 
