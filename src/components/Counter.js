1a32a68f3592d95ecc9ab63790cb7b731888f28c
MohsinShah12345 <mohsinking381@gmail.com>
2024-05-10 19:07:48 -0700

    Align Buttons Center

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './counter.css'
 import { useDispatch, useSelector } from 'react-redux'
 import { increment, decrement } from '../modules/counter/actions';
@@ -16,14 +16,40 @@ const Counter = () => {
         } else {
             alert("Count can't be less than 0")
         }
-
     }
     return (
-        <div className="container">
+        <div className="container" style={{
+            background: count % 2 == 0 ? "green" : "blue"
+        }}>
             <div className="count">{count || 0}</div>
             <div className="buttons">
                 <button onClick={addCount}>Increment</button>
                 <button onClick={subCount}>Decrement</button>
+            </div>
+            <div className='Increment' style={{
+                display: "flex",
+                flexWrap: 'wrap',// Optional: Center the buttons horizontally
+                justifyContent: "center",
+                margin: "auto"
+            }}>
+                {
+                    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => {
+                        return (
+                            <button key={_} style={{
+                                paddingTop: "1rem",
+                                paddingRight: "2rem",
+                                paddingBottom: "1rem",
+                                paddingLeft: "2rem",
+                                border: "2px solid red",
+                                borderRadius: "5px",
+                                fontSize: "15px",
+                                width: "170px",
+                                float: "centre",
+                                margin: "0.6rem"
+                            }}> Add</button>
+                        )
+                    })
+                }
             </div>
         </div >
     )
